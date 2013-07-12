c1e221ff9145c8f40085a7fbeb2d97bf0a423dff
Ubuntu <ubuntu@ip-172-31-16-150.us-west-2.compute.internal>
2013-07-12 06:57:15 +0000

    changed so it reads from file

--- web.js
+++ web.js
@@ -4,7 +4,11 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  // response.send('Hello World2!');
-    response.send(fs.readFileSync('index.html'));
+ //   response.send(fs.readFileSync('index.html'));
+fs.readFile('/etc/passwd', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
